c7a9260d65eac0ecb7144b524be5d93eaca95ede
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2019-04-27 09:19:01 +0530

    updates of django_db_creation as on 27/04/2019

--- portfolio/mysite/.ipynb_checkpoints/models-checkpoint.py
+++ portfolio/mysite/.ipynb_checkpoints/models-checkpoint.py
@@ -2,3 +2,19 @@ from django.db import models
 
 # Create your models here.
 # models.py is for to declare any kind of databse aspects
+# mysite_client
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.first_name
+    
+class Contact(models.Model):
+    email = models.EmailField()
+    subject = models.CharField(max_length=5000)
+    project_requirement = models.TextField()
+    
+    def __str__(self):
+        return self.email
