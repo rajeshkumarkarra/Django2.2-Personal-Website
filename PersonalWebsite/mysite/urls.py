4f7c0f063f48616f4338dd810adaecead154aec7
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2019-05-02 17:30:14 +0530

    adding models and forms

--- PersonalWebsite/mysite/urls.py
+++ PersonalWebsite/mysite/urls.py
@@ -18,5 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name ='index'),
+    path('portfolio', views.portfolio, name ='portfolio'),
+    path('contact', views.contact, name= 'contact'),
 ]
