d7ac659e6579afbb3869532835062bfc03121323
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2019-05-02 20:56:43 +0530

    adding json files and bootstrap content

--- PersonalWebsite/mysite/urls.py
+++ PersonalWebsite/mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name ='index'),
+    path('', views.index, name ='home'),
     path('portfolio', views.portfolio, name ='portfolio'),
     path('contact', views.contact, name= 'contact'),
 ]
